27ed78bbe4b0199a74a772a7cd5d8700a010183b
Johannes Klauss <klauss.johannes@gmail.com>
2022-05-27 11:23:47 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import {Box, Button, NativeBaseProvider} from 'native-base';
+import React from 'react';
+
+const App = () => {
+  return (
+    <NativeBaseProvider>
+      <Box>Hello</Box>
+    </NativeBaseProvider>
+  );
+};
+
+export default App;
